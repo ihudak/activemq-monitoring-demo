15cde6d85b20620aa6bbb8c546ec8b82435af97d
Ivan Gudak <ivan.gudak@dynatrace.com>
2025-12-26 18:36:13 +0100

    add logging

--- producer-app/src/main/java/eu/dec21/producer/service/MessageProducerService.java
+++ producer-app/src/main/java/eu/dec21/producer/service/MessageProducerService.java
@@ -3,12 +3,15 @@ package eu.dec21.producer.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class MessageProducerService {
 
     private final JmsTemplate jmsTemplate;
     private final String queueName;
+    private static final Logger log = LoggerFactory.getLogger(MessageProducerService.class);
 
     public MessageProducerService(JmsTemplate jmsTemplate,
                                   @Value("${app.queue-name}") String queueName) {
@@ -17,6 +20,7 @@ public class MessageProducerService {
     }
 
     public void sendText(String text) {
+        log.info("Sending message to queue: {}", text);
         jmsTemplate.convertAndSend(queueName, text);
     }
 }
--- producer-app/src/main/java/eu/dec21/producer/web/SendController.java
+++ producer-app/src/main/java/eu/dec21/producer/web/SendController.java
@@ -3,12 +3,15 @@ package eu.dec21.producer.web;
 import eu.dec21.producer.service.MessageProducerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/messages")
 public class SendController {
 
     private final MessageProducerService service;
+    private static final Logger log = LoggerFactory.getLogger(SendController.class);
 
     public SendController(MessageProducerService service) {
         this.service = service;
@@ -16,6 +19,7 @@ public class SendController {
 
     @PostMapping
     public ResponseEntity<String> send(@RequestBody String body) {
+        log.info("Preparing a message to queue: {}", body);
         service.sendText(body);
         return ResponseEntity.ok("Sent to queue: " + body);
     }
